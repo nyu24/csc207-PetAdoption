af1947377be0feac975e981fec26e346f7d88ac9
percivalfrancis <percy22francis@gmail.com>
2025-11-03 16:35:11 -0500

    Room and vet classes

---
+++ src/main/java/entities/Room.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+public class Room {
+
+    private List furnitureList;
+    private BufferedImage background;
+
+    public List getFurnitureList() {
+        return furnitureList;
+    }
+
+    public void setFurnitureList(List furnitureList) {
+        this.furnitureList = furnitureList;
+    }
+
+    public BufferedImage getBackground() {
+        return background;
+    }
+
+    public void setBackground(BufferedImage background) {
+        this.background = background;
+    }
+
+    private void drawBackground() {}
+}
