d04cba37a57beec6fe862260b22ff724f8a9d79a
waves <wavesj04@gmail.com>
2025-11-08 19:26:48 -0500

    Add default DAO objects and boilerplate code

---
+++ src/main/java/data_access/DBSaveDataAccessObject.java
@@ -0,0 +1,9 @@
+package data_access;
+
+import entities.Pet;
+import entities.User;
+import use_case.save_game.SaveGameUserDataAccessInterface;
+
+public class DBSaveDataAccessObject implements SaveGameUserDataAccessInterface {
+
+}
---
+++ src/main/java/data_access/InMemorySaveDataAccessObject.java
@@ -0,0 +1,7 @@
+package data_access;
+
+import use_case.save_game.SaveGameUserDataAccessInterface;
+
+public class InMemorySaveDataAccessObject implements SaveGameUserDataAccessInterface {
+    // To be implemented.
+}
