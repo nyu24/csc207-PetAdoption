55c4b0e6c35251bdb012c66e6e4ac9c02b9ea393
percivalfrancis <percy22francis@gmail.com>
2025-11-19 16:46:32 -0500

    The windows actually shows something.

--- src/main/java/interface_adapter/high_score/HighScoreViewModel.java
+++ src/main/java/interface_adapter/high_score/HighScoreViewModel.java
@@ -3,11 +3,11 @@ package interface_adapter.high_score;
 import interface_adapter.ViewModel;
 
 public class HighScoreViewModel extends ViewModel<HighScoreState> {
-    public static final String TITLE_LABEL = "List of High Scores";
+    public static final String TITLE_LABEL = "High Scores";
     public static final String CLOSE_BUTTON_LABEL = "Close";
 
     public HighScoreViewModel() {
-        super("List of High Scores");
+        super("High Scores");
         setState(new HighScoreState());
     }
 
--- src/main/java/view/HighScoreView.java
+++ src/main/java/view/HighScoreView.java
@@ -1,6 +1,9 @@
 package view;
 
+import interface_adapter.high_score.HighScoreViewModel;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -10,7 +13,26 @@ public class HighScoreView extends JPanel implements ActionListener, PropertyCha
     private final String viewName = "High Scores";
 
     private final JButton closeView = new JButton("Close");
-    public HighScoreView() {
+    private HighScoreViewModel highScoreViewModel;
+
+    // private controller...
+
+    private final JButton close;
+    public HighScoreView(HighScoreViewModel highScoreViewModel) {
+        this.highScoreViewModel = highScoreViewModel;
+        highScoreViewModel.addPropertyChangeListener(this);
+        final JLabel title = new JLabel(HighScoreViewModel.TITLE_LABEL);
+
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        final JPanel buttons = new JPanel();
+        close = new JButton(HighScoreViewModel.CLOSE_BUTTON_LABEL);
+        buttons.add(close);
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(title);
+        this.add(buttons);
 
     }
     @Override
