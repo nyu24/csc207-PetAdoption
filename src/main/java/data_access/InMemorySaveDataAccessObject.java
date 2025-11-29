62fd6b5dddd99048f2c61f9649e0b1006c047906
waves <wavesj04@gmail.com>
2025-11-29 13:38:42 -0500

    Add in memory DAO for testing savedata

---
+++ src/main/java/data_access/InMemorySaveDataAccessObject.java
@@ -0,0 +1,27 @@
+package data_access;
+
+import entities.SaveFile;
+import use_case.load_game.LoadGameDataAccessInterface;
+import use_case.save_game.SaveGameDataAccessInterface;
+
+/**
+ * In-memory implementation of the DAO for storing save game data. This implementation does
+ * not persist data between runs of the program and is solely used for testing purpose.
+ */
+
+public class InMemorySaveDataAccessObject implements SaveGameDataAccessInterface, LoadGameDataAccessInterface {
+    private SaveFile saveFile;
+
+    public void save(SaveFile saveFile) {
+        this.saveFile = saveFile;
+    }
+
+    public SaveFile load() {
+        return this.saveFile;
+    }
+
+    @Override
+    public boolean saveDataExists() {
+        return saveFile != null;
+    }
+}
