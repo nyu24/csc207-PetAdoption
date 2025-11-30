a63c79ecfe3630cb2ee9786ae02041cb5b29e687
waves <wavesj04@gmail.com>
2025-11-30 12:48:43 -0500

    Minor comment edits and deleting useless code

--- src/main/java/use_case/save_game/SaveGameInputBoundary.java
+++ src/main/java/use_case/save_game/SaveGameInputBoundary.java
@@ -3,7 +3,5 @@ package use_case.save_game;
 public interface SaveGameInputBoundary {
     void execute(SaveGameInputData saveGameInputData);
 
-    void execute();
-
     void switchToPetRoomView();
 }
