aee3cc880264385ae36ea6a8abe140916832dc63
kriebelg <35kriebelg@gmail.com>
2025-12-01 16:32:41 -0500

    Fix check style

--- src/main/java/entities/Constants.java
+++ src/main/java/entities/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     public static final int SAD_THRESHOLD = 60;
     public static final int MAIN_GAIN = 15;
     public static final int SUB_GAIN = 5;
-    public static final int SMALL_GAIN = 3;
+    public static final int SMALL_GAIN = 2;
 
     public static final int MAX_SHOWN_SCORES = 10;
 }
